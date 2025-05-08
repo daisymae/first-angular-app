5470173c0655359621a06fd4be83fbdd8f01e0af
daisymae <daisymae.orcadiafarm@gmail.com>
2025-05-08 14:36:01 -0500

    Adding more users
    Adding users to list.
    Setting input to be required.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
-
+import { DUMMY_USERS } from './dummy-users';
 @Component({
   selector: 'app-root',
   imports: [HeaderComponent, UserComponent],
@@ -10,4 +10,5 @@ import { UserComponent } from "./user/user.component";
 })
 export class AppComponent {
   title = 'first-angular-app';
+  users = DUMMY_USERS;
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-user',
   imports: [],
@@ -9,22 +6,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  // computed sets up a listener to the signal so whenever it changes, the value will be recomputed
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
-  // zone way:
-  // selectedUser = DUMMY_USERS[randomIndex]
+  // the '!' tells Angular we know it will be set
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
-  // zone way of getting the data
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    // signal way
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-    // zone.js way (default)
-    // this.selectedUser = DUMMY_USERS[randomIndex];
   }
 }
