68198a9241572b9cf64dd4e8fe7b468a82f19fab
daisymae <daisymae.orcadiafarm@gmail.com>
2025-05-08 17:41:00 -0500

    For and If
    Older ways to use for and if. Removing for newer ways.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,17 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from "./tasks/tasks.component";
+import { NgFor, NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first-angular-app';
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)!;
