e6a0ff06551f6ae7b3a4ee8292b3753d770c1e12
daisymae <daisymae.orcadiafarm@gmail.com>
2025-05-08 14:48:55 -0500

    input
    How to do inputs with signals support.
    Will remove this going forward, but want to document.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
 @Component({
   selector: 'app-user',
   imports: [],
@@ -7,12 +7,26 @@ import { Component, Input } from '@angular/core';
 })
 export class UserComponent {
   // the '!' tells Angular we know it will be set
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
 
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
-  }
+  // how to handle inputs with signals
+  // can give a default value in the () here too
+  // avatar = input<string>();
+  // alternatively, to make required; no default allowed here because marked as required
+  // HTML inputs doesn't change, but do need to change the user component HTML to use signals
+  //  these input signal values are readonly; cannot be modified within the component
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  // and imagePath should be updated to computed now too
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar();
+  });
+
+  // get imagePath() {
+  //   return 'assets/users/' + this.avatar;
+  // }
 
   onSelectUser() {
   }
