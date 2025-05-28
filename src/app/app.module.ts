652b41471841acb614eaeab898180a2e4f206b7e
daisymae <daisymae.orcadiafarm@gmail.com>
2025-05-27 21:59:00 -0500

    All Modules
    Turn all components into Modules. Not recommended now, but is something that could see in existing code, and is still possible.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+
 import { AppComponent } from "./app.component";
 import { TasksComponent } from "./tasks/tasks.component";
 import { UserComponent } from "./user/user.component";
 import { HeaderComponent } from './header/header.component';
-import { BrowserModule } from "@angular/platform-browser";
+import { CardComponent } from "./shared/card/card.component";
+import { TaskComponent } from "./tasks/task/task.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 
 // Hmmm... he said BrowserModule is required, but my app was working without it??
+// While Modules CAN be done, not really recommended anymore. Standalone is easier to track dependencies.
 @NgModule({
   // declarations canNOT be standalone components
-  declarations: [AppComponent],
+  declarations: [AppComponent,
+    HeaderComponent,
+    UserComponent,
+    CardComponent,
+    TasksComponent,
+    TaskComponent, // DatePipe is included in BrowserModule and not needed here
+    NewTaskComponent
+  ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent] // imports can be used for standalone components
+  imports: [BrowserModule, FormsModule] // imports can be used for standalone components
 })
 export class AppModule {}
